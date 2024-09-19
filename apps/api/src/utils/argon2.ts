e46cb08f944a13093fbc4e467551c970c58cd589
Abdo Nasser <abdonasser865@gmail.com>
2024-09-19 18:01:26 +0300

    feat(auth): add hashing functionality using argon2

---
+++ apps/api/src/utils/argon2.ts
@@ -0,0 +1,11 @@
+import * as argon2 from 'argon2';
+
+export async function hash(password: string) {
+    const hashedPassword = await argon2.hash(password);
+    return hashedPassword;
+}
+
+export async function verify(original: string, hash: string) {
+    const isMatch = await argon2.verify(hash, original);
+    return isMatch;
+}
