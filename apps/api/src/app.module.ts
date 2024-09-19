69d6e2be2be959981eea901d5b408627ceccd6ad
Abdo Nasser <abdonasser865@gmail.com>
2024-09-19 06:40:51 +0300

    feat(auth): add structure of auth with config module added

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [],
+    imports: [
+        AuthModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
