e005309fd31f0837ad81d30f3421e758cb80d08a
Mohammed Ashraf <mohammedashraf4599@gmail.com>
2024-09-21 01:10:30 +0300

    style: formate the code

--- apps/api/src/app.resolver.ts
+++ apps/api/src/app.resolver.ts
@@ -1,9 +1,9 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Query, Resolver } from "@nestjs/graphql";
 
 @Resolver()
 export class AppResolver {
     @Query(() => String)
     hello(): string {
-        return 'Hello World!';
+        return "Hello World!";
     }
 }
--- apps/api/src/drizzle/drizzle.config.ts
+++ apps/api/src/drizzle/drizzle.config.ts
@@ -1,12 +1,12 @@
-import { ConfigService } from '@nestjs/config';
-import { defineConfig } from 'drizzle-kit';
+import { ConfigService } from "@nestjs/config";
+import { defineConfig } from "drizzle-kit";
 
-const databaseUrl = new ConfigService().get('DATABASE_URL');
+const databaseUrl = new ConfigService().get("DATABASE_URL");
 
 export default defineConfig({
-    dialect: 'postgresql',
-    schema: './src/drizzle/schema.ts',
-    out: './src/drizzle/migrations',
+    dialect: "postgresql",
+    schema: "./src/drizzle/schema.ts",
+    out: "./src/drizzle/migrations",
     dbCredentials: {
         url: databaseUrl,
     },
