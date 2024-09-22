6105e2fb529229e6d400dc56eb5ba8028c1b358d
Mohammed Ashraf <125150223+Holo-xy@users.noreply.github.com>
2024-09-22 16:46:16 +0300

    feat: initialize drizzle orm (#11)

---
+++ apps/api/src/drizzle/drizzle.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from "@nestjs/config";
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = new ConfigService().get("DATABASE_URL");
+
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/drizzle/schema.ts",
+    out: "./src/drizzle/migrations",
+    dbCredentials: {
+        url: databaseUrl,
+    },
+});
