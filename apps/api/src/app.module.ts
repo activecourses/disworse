8149f43d28f345f9dd5b73aaa936ab781bf97053
Mohammed Ashraf <125150223+Holo-xy@users.noreply.github.com>
2024-09-22 12:52:11 +0300

    feat: initialize graphql (#9)
    * feat: initialize graphql
    * style: format the code
    
    ---------
    
    Co-authored-by: Ali Kehel <ali.saber.27.2@gmail.com>

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,19 @@
+import { join } from "path";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
 import { AppController } from "./app.controller";
+import { AppResolver } from "./app.resolver";
 import { AppService } from "./app.service";
 
 @Module({
-    imports: [],
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+        }),
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, AppResolver],
 })
 export class AppModule {}
