15703f70a489f655416cb78f2a239efcd7a4b241
Abdo Nasser <abdonasser865@gmail.com>
2024-09-26 16:25:57 +0300

    chore: add tag to drizzle module and correction on the user schema

--- apps/api/src/drizzle/drizzle.module.ts
+++ apps/api/src/drizzle/drizzle.module.ts
@@ -6,6 +6,7 @@ import * as schema from "./schema";
 @Module({
     imports: [
         DrizzlePGModule.registerAsync({
+            tag: "DB_DEV",
             useFactory: (configService: ConfigService) => ({
                 pg: {
                     connection: "pool",
--- apps/api/src/drizzle/schema/user.ts
+++ apps/api/src/drizzle/schema/user.ts
@@ -9,13 +9,13 @@ import { userChats } from "./user-chats";
 export const users = pgTable("users", {
     id: serial("id").primaryKey(),
     username: varchar("username", { length: 48 }).notNull().unique(),
-    fullName: varchar("full_name", { length: 64 }).notNull(),
+    name: varchar("full_name", { length: 64 }).notNull(),
     email: varchar("email", { length: 64 }).notNull().unique(),
+    password: varchar("password", { length: 128 }).notNull(),
     bio: varchar("bio", { length: 255 }),
     dob: date("dob").notNull(), // date of birth
     profile_image_url: varchar("profile_image_url", { length: 255 }),
     cover_image_url: varchar("cover_image_url", { length: 255 }),
-    session: varchar("refresh_token", { length: 128 }),
     google_id: varchar("google_id", { length: 128 }),
     github_id: varchar("github_id", { length: 128 }),
     created_at: date("created_at").defaultNow(),
