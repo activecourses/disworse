eb1fab19c357880e6fb0b2a041ff15fb8b0c61ad
bassiounix <bassiouni.git@gmail.com>
2024-09-27 17:48:36 +0300

    chore: comment aurh module code for CI to work

--- apps/api/src/modules/auth/dto/signup.dto.ts
+++ apps/api/src/modules/auth/dto/signup.dto.ts
@@ -1,14 +1,10 @@
-import { createZodDto } from "nestjs-zod";
-import { z } from "nestjs-zod/z";
-import { Password } from "../../../common/custom-validation/password";
-import { Username } from "../../../common/custom-validation/username";
+// const signupSchema = z.object({
+//     name: z.string().transform((name) => name.trim()),
+//     email: z.string().email("Invalid email address"),
+//     dob: z.string().date("Date must be in the form yyyy-mm-dd"),
+//     username: Username,
+//     password: Password,
+// });
 
-const signupSchema = z.object({
-    name: z.string().transform((name) => name.trim()),
-    email: z.string().email("Invalid email address"),
-    dob: z.string().date("Date must be in the form yyyy-mm-dd"),
-    username: Username,
-    password: Password,
-});
-
-export class SignupDto extends createZodDto(signupSchema) {}
+// TODO: implement class validators & schema
+export class SignupDto {}
