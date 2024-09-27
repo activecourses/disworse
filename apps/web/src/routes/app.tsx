066a24d8e159a4bad2b4ec4aa2ca5334ffbb5b98
Ali Kehel <ali.saber.27.2@gmail.com>
2024-09-27 09:31:08 +0300

    fix: put outlet in app root

--- apps/web/src/routes/app.tsx
+++ apps/web/src/routes/app.tsx
@@ -1,7 +1,6 @@
 import { Sidebar } from "@/components/layouts/sidebar";
-import { Logo } from "@/components/ui/logo";
 
-import { createFileRoute } from "@tanstack/react-router";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app")({
     component: AppRoot,
@@ -11,12 +10,7 @@ function AppRoot() {
     return (
         <>
             <Sidebar />
-            <div className="flex h-screen flex-col items-center justify-center">
-                <Logo alt="Logo" className="h-24 w-24" />
-                <h1 className="mt-4 font-bold text-4xl text-foreground">
-                    App Root
-                </h1>
-            </div>
+            <Outlet />
         </>
     );
 }
