d2a735e2ab032b59d2ea8a31e850f05ba6432f41
Abdo Nasser <abdonasser865@gmail.com>
2024-09-28 23:05:08 +0300

    feat(auth): add auth guard and login endpoint

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import { Request } from "express";
+import { LocalAuthGuard } from "src/common/guards/local-auth.guard";
 import { SerializedUser } from "src/common/serialized-types/user";
+import { AuthenticatedGuard } from "../../common/guards/auth.guard";
 import { AuthService } from "./auth.service";
 import { SignupDto } from "./dto/signup.dto";
 
@@ -12,5 +15,11 @@ export class AuthController {
         const user = await this.authService.signup(signupDto);
 
         return new SerializedUser(user);
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post("login")
+    async login(@Req() req: Request) {
+        return req.user;
     }
 }
