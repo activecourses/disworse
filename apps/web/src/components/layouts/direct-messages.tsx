a3d79aad5452d7cc50ed30b8bfdd5499242d6b66
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2024-09-29 03:13:19 +0300

    fix: remove borders and fix a responsive design issue

--- apps/web/src/components/layouts/direct-messages.tsx
+++ apps/web/src/components/layouts/direct-messages.tsx
@@ -5,11 +5,11 @@ import { OnlineStatus } from "../ui/status/online";
 
 export const DirectMessages = () => {
     return (
-        <nav className="ml-20 flex h-screen w-[240px] flex-col bg-zinc-800 text-white">
+        <nav className="flex h-screen w-full flex-col bg-zinc-800 pl-20 text-white md:w-[320px]">
             {/* Search bar */}
             <div className="flex h-12 items-center justify-center border-black border-b py-3 ">
                 <input
-                    className="w-5/6 rounded-md border-2 border-black border-solid bg-transparent px-4 py-1 text-white text-xs outline-none transition-colors duration-100 focus:border-[#596A95]"
+                    className="w-5/6 rounded-md border-2 border-zinc-700 border-solid bg-transparent px-4 py-1 text-white text-xs outline-none transition-colors duration-100 focus:border-black"
                     name="text"
                     placeholder="Find or start a conversation"
                     type="text"
@@ -21,28 +21,7 @@ export const DirectMessages = () => {
                     to="/app/channels/me"
                     className="mt-2 mb-2 flex h-14 cursor-pointer items-center gap-2 rounded-md px-2 py-2 font-semibold opacity-80 hover:bg-zinc-900"
                 >
-                    <Logo
-                        className="h-10 w-10 rounded-full border-2 border-zinc-800 bg-zinc-900 p-2 "
-                        alt="Friend"
-                    />
-                    {/* <svg
-                    aria-hidden="true"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                    <path
-                        fill="currentColor"
-                        d="M13 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z"
-                    ></path>
-                    <path
-                        fill="currentColor"
-                        d="M3 5v-.75C3 3.56 3.56 3 4.25 3s1.24.56 1.33 1.25C6.12 8.65 9.46 12 13 12h1a8 8 0 0 1 8 8 2 2 0 0 1-2 2 .21.21 0 0 1-.2-.15 7.65 7.65 0 0 0-1.32-2.3c-.15-.2-.42-.06-.39.17l.25 2c.02.15-.1.28-.25.28H9a2 2 0 0 1-2-2v-2.22c0-1.57-.67-3.05-1.53-4.37A15.85 15.85 0 0 1 3 5Z"
-                    ></path>
-                </svg> */}
+                    <Logo className="h-10 w-10 rounded-full" alt="Friend" />
                     Friends
                 </Link>
                 {/* Private Chats */}
@@ -62,7 +41,7 @@ export const DirectMessages = () => {
                                 <div className="relative">
                                     {/* Friend Image */}
                                     <Logo
-                                        className="h-10 w-10 rounded-full border-2 border-zinc-800 bg-zinc-900 p-2 "
+                                        className="h-10 w-10 rounded-full"
                                         alt="Friend"
                                     />
                                     {/* Friend Status */}
