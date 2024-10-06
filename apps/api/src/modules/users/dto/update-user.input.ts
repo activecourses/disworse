f58d9aac78c0adedc853701666f6e479885d0dd2
Abdo Nasser <abdonasser865@gmail.com>
2024-10-06 11:30:43 +0300

    feat(users): add input examples for create and update

---
+++ apps/api/src/modules/users/dto/update-user.input.ts
@@ -0,0 +1,8 @@
+import { Field, InputType, Int, PartialType } from "@nestjs/graphql";
+import { CreateUserInput } from "./create-user.input";
+
+@InputType()
+export class UpdateUserInput extends PartialType(CreateUserInput) {
+    @Field(() => Int)
+    id: number;
+}
