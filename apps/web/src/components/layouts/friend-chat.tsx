bc70c45ef924dea97383f92a97ec55bf576fd339
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2024-10-17 12:05:15 +0300

    feat: add reacts to the message

--- apps/web/src/components/layouts/friend-chat.tsx
+++ apps/web/src/components/layouts/friend-chat.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "@/components/ui/logo";
+import { Message } from "@/components/ui/messages/message";
 import { useEffect, useRef } from "react";
 
 export const FriendChat = ({ id }: { id: number }) => {
@@ -13,30 +13,9 @@ export const FriendChat = ({ id }: { id: number }) => {
         >
             <div className="flex flex-col gap-4">
                 {Array.from({ length: 25 }).map((_, index) => (
-                    <div
-                        className="flex cursor-default gap-2 px-5 py-2 hover:bg-zinc-800/50"
-                        key={index}
-                    >
-                        <Logo
-                            alt="Friend"
-                            className="h-10 w-10 rounded-full hover:rounded-full"
-                        />
-                        <div className="w-full">
-                            <div className="flex items-center gap-2">
-                                <h2 className="font-bold text-white">
-                                    {index % 2 === 0
-                                        ? `Friend ${id}`
-                                        : `TheGoatt`}
-                                </h2>
-                                <p className="text-white/40 text-xs">
-                                    {new Date().toLocaleString()}
-                                </p>
-                            </div>
-                            <p>Welcome this is a message number {index}</p>
-                        </div>
-                    </div>
+                    <Message key={index} id={id.toString()} index={index} />
                 ))}
-                <div className="sticky bottom-0 w-full px-2">
+                <div className="sticky bottom-0 w-full px-2 pt-3">
                     <input
                         className="w-full rounded-md border-2 border-zinc-700 border-solid bg-zinc-700 px-4 py-3 text-white text-xs outline-none"
                         name="text"
