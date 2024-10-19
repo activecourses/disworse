a8f61e2852b1d1967301d50e45fda08003412811
ayman <aymanshalaby539@gmail.com>
2024-10-19 16:03:10 +0300

    fix: add depen and fix auth controller

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,16 +1,34 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { SerializedUser } from "../../common/serialized-types/user";
+import { UseGuards } from "@nestjs/common";
+import { Args, Context, Mutation, Resolver } from "@nestjs/graphql";
+import { Request, Response } from "express";
+import { Public } from "../../common/custom-decorators/public-endpoint";
+import { LocalAuthGuard } from "../../common/guards/local-auth.guard";
+import { User } from "../users/entities/user.entity";
 import { AuthService } from "./auth.service";
-import { SignupDto } from "./dto/signup.dto";
+import { LoginDto } from "./dto/login.input";
+import { SignupDto } from "./dto/signup.input";
 
-@Controller("auth")
-export class AuthController {
+@Resolver("auth")
+export class AuthResolver {
     constructor(private readonly authService: AuthService) {}
 
-    @Post("signup")
-    async signup(@Body() signupDto: SignupDto) {
+    @Public()
+    @Mutation(() => User, { name: "signup" })
+    async signup(@Args() signupDto: SignupDto) {
         const user = await this.authService.signup(signupDto);
 
-        return new SerializedUser(user);
+        return user;
+    }
+
+    @Public()
+    @UseGuards(LocalAuthGuard)
+    @Mutation(() => User, { name: "login" })
+    async login(@Context() ctx: any, @Args() loginDto: LoginDto) {
+        return ctx.req.user;
+    }
+
+    @Mutation(() => Boolean, { name: "logout" })
+    async logout(@Context() context: { req: Request; res: Response }) {
+        return await this.authService.logout(context.req, context.res);
     }
 }
