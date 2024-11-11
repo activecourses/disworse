ecbebe3084e90beb5c3948d9a6487f5ad1b7f24a
Ali Kehel <ali.saber.27.2@gmail.com>
2024-11-11 19:24:01 +0200

    fix: :bug: add flex class to app/channels layout

--- apps/web/src/routes/_auth.app.channels.tsx
+++ apps/web/src/routes/_auth.app.channels.tsx
@@ -2,7 +2,7 @@ import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth/app/channels")({
     component: () => (
-        <div className="flex-grow">
+        <div className="flex flex-grow">
             <Outlet />
         </div>
     ),
